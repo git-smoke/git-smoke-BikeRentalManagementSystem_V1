c9a8a2f97e4eea908a2d0142a4d1b1f93737943e
Sankeerthan D. <UT01235@unicomtic.com>
2024-10-11 09:51:51 +0530

    Question-02

---
+++ Bike.cs
@@ -0,0 +1,14 @@
+﻿namespace BikeRentalManagementSystem_V1
+{
+    public class Bike
+    {
+        private string BikeId { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private decimal RentalPrice { get; set; }
+
+        
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace BikeRentalManagementSystem_V1{
-    public class Program{
-        static void Main(string[] args){
+namespace BikeRentalManagementSystem_V1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine("Hello World");
         }
     }
